f753ae0b399519b5acb76f0ef28d033a40a07b35
Vradash <singhvikramrajan111@gmail.com>
2024-06-16 10:42:06 +0530

    feat: Add MobileNav and SideBar components to RootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,30 @@
+import MobileNav from "@/components/MobileNav";
+import SideBar from "@/components/Sidebar"
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Vikram", lastName: "Singh"};
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn}/>
+      <div className="flex_size-full_flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" 
+          width={30}
+          height={30}
+          alt="menu icon"/>
+          <div>
+            <MobileNav 
+              user={loggedIn}
+            />
+          </div>
+        </div>
+      {children}
+      </div>
     </main>
   );
 }
