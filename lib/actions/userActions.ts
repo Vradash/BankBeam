703730242351940c1ba64abf7243adb7d6c6e4b6
Vradash <singhvikramrajan111@gmail.com>
2024-06-21 14:41:51 +0530

    refactor: Add sign-in and sign-up actions to userActions.ts

---
+++ lib/actions/userActions.ts
@@ -0,0 +1,17 @@
+'use server'
+
+export const signIn = async()=>{
+    try {
+        
+    } catch (err) {
+        console.log("Error",err);
+    }
+
+}
+export const signUp = async(userData : SignUpParams)=>{
+    try {
+        
+    } catch (err) {
+        console.log("Error",err);
+    }
+}
