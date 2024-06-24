fb6cb5a5454c7aad4a5ce706f09e3653165c3fbc
Vradash <singhvikramrajan111@gmail.com>
2024-06-24 12:17:06 +0530

    refactor: Update BankCard component to display only the user's name instead of the account name

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 import BankCard from './BankCard'
 
 const RightSidebar = ({user,transactions,banks}:RightSidebarProps) => {
+    // console.log(user)
+    // user="Vikram";
   return (
     // Not sure about below classname is to be checked
     <aside className="right-sidebar absolute right-0">
@@ -12,13 +14,13 @@ const RightSidebar = ({user,transactions,banks}:RightSidebarProps) => {
             <div className="profile">
                 <div className="profile-img">
                     <span className="text-5xl font-bold text-blue-500">
-                        {user.firstName[0]}
+                        {user.name[0]}
                     </span>
                 </div>
 
                 <div className="profile-details">
                     <h1 className="profile-name">
-                        {user.firstName} {user.lastName}
+                        {user.name}
                     </h1>
                     <p className="profile-email">
                         {user.email}
@@ -50,7 +52,7 @@ const RightSidebar = ({user,transactions,banks}:RightSidebarProps) => {
                         <BankCard 
                             key={banks[0].$id}
                             account={banks[0]}
-                            userName={`${user.firstName} ${user.lastName}`}
+                            userName={user.name}
                             showBalance={false}
                         />
                     </div>
@@ -59,7 +61,7 @@ const RightSidebar = ({user,transactions,banks}:RightSidebarProps) => {
                             <BankCard 
                                 key={banks[1].$id}
                                 account={banks[1]}
-                                userName={`${user.firstName} ${user.lastName}`}
+                                userName={user.name}
                                 showBalance={false}
                             />
                         </div>
