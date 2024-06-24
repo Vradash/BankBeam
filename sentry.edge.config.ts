3abca9e15ac4d494287949879af4ae23e0c32589
Vradash <singhvikramrajan111@gmail.com>
2024-06-24 12:20:07 +0530

    refactor: Add @sentry/nextjs package for error monitoring

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6466b74e06f5d40bb389fa8619584ec8@o4507485396795392.ingest.us.sentry.io/4507485403938816",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
