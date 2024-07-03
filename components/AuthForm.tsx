d38f5aef89ce0c08519aa706e9b4eac0b3bb9109
Vradash <singhvikramrajan111@gmail.com>
2024-07-03 16:41:48 +0530

    refactor: Add PlaidLink component to AuthForm for linking bank accounts

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,6 +24,7 @@ import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import SignIn from '@/app/(auth)/sign-in/page'
 import { signIn, signUp } from '@/lib/actions/userActions'
+import PlaidLink from './PlaidLink'
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter();
@@ -46,8 +47,21 @@ const AuthForm = ({ type }: { type: string }) => {
         setIsLoading(true);
         try{
             console.log(data);
+            
             if(type==='sign-up'){
-                const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password
+                }
+                const newUser = await signUp(userData);
                 setUser(newUser);
             }
             if(type==='sign-in'){
@@ -88,9 +102,9 @@ const AuthForm = ({ type }: { type: string }) => {
                     </p>
                 </div>
             </header>
-            {user ? (
+            {user ? ( 
                 <div className="flex flex-col gap-4">
-
+                    <PlaidLink user={user} variant="primary"/>
                 </div>
             ) : (
                 <>
@@ -168,9 +182,9 @@ const AuthForm = ({ type }: { type: string }) => {
                             {type==="sign-in"? "Sign up": "Sign in"}
                         </Link>
                     </footer>
-                </>)
-
-            }
+                </>
+                ) 
+            } 
         </section>
     )
 }
